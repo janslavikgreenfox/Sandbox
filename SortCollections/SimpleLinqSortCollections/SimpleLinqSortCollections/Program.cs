25f2146af7501770d7c7f4a4d88455cc2ee19d22
Jan Slavik <janslavikgreenfox@gmail.com>
2021-01-12 10:06:16 +0100

    A sort example/trial by the Array.Sort method

---
+++ SortCollections/SimpleLinqSortCollections/SimpleLinqSortCollections/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace SimpleLinqSortCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string[] test1 = new string[] { "H", "A", "L", "O", "E" };
+            foreach(var item in SortedByArraySort(test1))
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (var item in GenericSort<string>(test1))
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static int[] SortedByArraySort(int[] inputToSort)
+        {
+            int [] sortedOutput = inputToSort;
+            Array.Sort(sortedOutput);
+            return sortedOutput;
+        }
+
+        static string[] SortedByArraySort(string[] inputToSort)
+        {
+            string[] sortedOutput = inputToSort;
+            Array.Sort(sortedOutput);
+            return sortedOutput;
+        }
+
+       static  T[] GenericSort<T>(T[] inputToSort) where T : IComparable
+        {
+            T[] sortedOutput = inputToSort;
+            Array.Sort(sortedOutput);
+            return sortedOutput;
+        }
+    }
+}
