6c251a639ee053f6091eaa0ffd3b23d175644729
Jan Slavik <janslavikgreenfox@gmail.com>
2021-01-25 00:22:09 +0100

    Fix the Get method in the CreateHTTP request project.

--- CreateHTTPRequest/CreateHttpRequest/CreateHttpRequest/Program.cs
+++ CreateHTTPRequest/CreateHttpRequest/CreateHttpRequest/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
 
 namespace CreateHttpRequest
 {
     class Program
     {
+        public static object JsonConvert { get; private set; }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var client = new HttpClient();
+            //var wiki = new Uri("https://www.google.com/");
+            var simple = new Uri("https://www.fi.muni.cz/~xjanu/about-website.html");
+
+            var result = client.GetAsync(simple).Result;
+            var content = result.Content.ReadAsStringAsync().Result;
+            
+            //var res = JsonSerializer.Deserialize<List<string>>(content);
+            foreach(var item in content.Split("\n"))
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
